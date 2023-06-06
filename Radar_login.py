f6c9ef5ec4212df4985971c4108ec4922c954ef8
Cicco77 <76960225+Cicco77@users.noreply.github.com>
2023-06-06 22:18:01 +0200

    Create Radar_login.py
    1st commitment

---
+++ Radar_login.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python
+
+import requests
+import json
+
+
+radarendpointurl = 'https://radar.wandera.com'
+radarusername = #'write_here_your_username'
+radarpassword = #'write_here_your_username_password'
+childaccountid = #'write_here_your_radar_portalid'
+
+# do not modify the below
+global radartoken           #this token will be used for all API requests
+
+
+def loadradar():
+    print('Attempting to login to Radar with ' + radarusername + ' user.')
+    noauthheaders = {'Accept': 'application/json, text/javascript, */*; q=0.01', 'Content-Type': 'application/json'}
+    loadcredentialsurl = radarendpointurl + '/auth/v4/getToken'
+    payload = {
+                'username': radarusername,
+                'password': radarpassword,
+                'sessionId': '28ece5078288ab76867b26244ebd4a39', #random number
+                'remember': 'true', #needed
+                'totp': '', #needed
+                'backupCode': '' #needed
+            }
+   
+    try:
+        r = requests.post(loadcredentialsurl, headers=noauthheaders, json=payload)
+
+    except:
+        print('Error logging into Radar API - exception')
+        os._exit(1)
+
+    if (r.text.startswith('{"code"')):
+        print('Error logging into Radar API - code' + r.text)
+        os._exit(1)
+    
+    responsejson = json.loads(r.text)
+    token = (responsejson['token'])
+    radartoken = 'Bearer ' + token
+    print('Your Radar token for further API scraping is:\n' +radartoken)
+
+
+if __name__ == "__main__":
+    loadradar()
